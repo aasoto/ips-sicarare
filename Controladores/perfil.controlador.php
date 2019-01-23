8dbc114e03a1b44dd43cb51726e48dc15fe3bf87
Eduard Diaz <eecastellar@unicesar.edu.co>
2019-01-22 22:50:50 -0500

    piedad

---
+++ Controladores/perfil.controlador.php
@@ -0,0 +1,26 @@
+<?php 
+	/**
+	 * 
+	 */
+	class perfilcontrolador
+	{
+		
+		private $modelo;
+
+	    public function __CONSTRUCT(){
+
+	    }
+
+	    public function Inicio(){
+	        require_once "Vistas/encabezado.php";
+	        require_once "Vistas/perfil/index.php";
+	        require_once "Vistas/pie.php";
+	    }
+	}
+
+
+
+
+
+
+ ?>
---
+++ perfil/index.php
@@ -0,0 +1,84 @@
+<main class="app-content">
+    <div class="app-title">
+        <div>
+          <h1><i class="fa fa-cog fa-lg"></i> Cambio de Contraseña</h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+          <li class="breadcrumb-item"><a href="#"><i class="fa fa-home fa-lg"></i></a></li>
+          <li class="breadcrumb-item"><a href="partida.php">inicio</a></li>
+        </ul>
+    </div>
+    <div class="col-md-12">
+          <div class="tile">
+            <h3 class="tile-title">Actualice su contraseña</h3>
+            <div class="tile-body">
+                <form class="form-horizontal" method="post" action="?c=medico&a=Guardar" role="form">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-4 col-form-label">Contraseña Actual</label><br>
+                                    <div class="col-sm-8">
+                                      <input type="text" name="numdoc" class="form-control" id="nomdoc" placeholder="Número de Identificación" required> <br>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-4 col-form-label">Ingrese su nueva contraseña</label>
+                                    <div class="col-sm-8">
+                                      <input type="text" name="nom2" class="form-control" id="nom2" placeholder="Segundo Nombre">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-4 col-form-label">Repita su nueva contraseña</label>
+                                    <div class="col-sm-8">
+                                      <input type="text" name="apellido2" required="" class="form-control" id="apellido2" placeholder="Segundo Apellido">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-6">
+                        
+                       
+                    </div>
+                    
+                    
+                    <div class="tile-footer">       
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-fw fa-lg fa-paper-plane"></i>Actualizar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    </main>
